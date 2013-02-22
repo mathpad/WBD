c3adc5bf07858d521edf3313a651ef3102446deb
ed <ed@darnell.org.uk>
2013-02-22 20:51:39 +0000

    hide

--- WBD/src/uk/org/darnell/wbd/FullscreenActivity.java
+++ WBD/src/uk/org/darnell/wbd/FullscreenActivity.java
@@ -124,7 +124,7 @@ public class FullscreenActivity extends Activity {
    
         gv = new GraphicsView(this);
         setContentView(gv);
-        
+        mSystemUiHider.hide();
     }
 
     @Override
