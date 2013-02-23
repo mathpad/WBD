d2a40e471a7d57af5e5e13cf2a335a09f30937b8
ed <ed@darnell.org.uk>
2013-02-23 17:34:14 +0000

    Added comms and drawing

--- WBD/src/uk/org/darnell/socket/Server.java
+++ WBD/src/uk/org/darnell/socket/Server.java
@@ -2,25 +2,16 @@ package uk.org.darnell.socket;
 import java.io.*;
 import java.net.*;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 
 public class Server{
-	public static Bitmap screen;
 	static ServerSocket providerSocket;
 	static Socket connection = null;
 	static ObjectOutputStream out;
 	static ObjectInputStream in;
-	String message;
-	private final String TAG = "Server.java";
-	static byte nextPoint = 0;
-	static byte[] points = new byte[100];
-	static int lastX = 0;
-	static int lastY = 0;
-	static boolean toSend = false;
+	private final String TAG = "WBD Server";
 
 	public Server(){}
 	public void run(Handler handler)
@@ -43,23 +34,8 @@ public class Server{
 			// sendMessage("Connection successful");
 			//4. The two parts communicate via the input and output streams
 			do{
-				try{
-					message = (String)in.readObject();
-					System.out.println("client>" + message);
-					handler.sendMessage(handler.obtainMessage(1,0,0,"Message from client>" + message));
-					if (message.equals("screen"))
-					{
-						byte[] screendata = (byte[])in.readObject();
-						screen = BitmapFactory.decodeByteArray(screendata, 0, screendata.length);
-						handler.sendMessage(handler.obtainMessage(2,0,0,"screen data:" + screendata.length));
-					}
-					if (message.equals("bye"))
-						sendMessage("bye");
-				}
-				catch(ClassNotFoundException classnot){
-					System.err.println("Data received in unknown format");
-				}
-			}while(!message.equals("bye"));
+					readMessage(handler);
+			}while(true);
 		}
 		catch(IOException ioException){
 			handler.sendMessage(handler.obtainMessage(1,0,0,"Connection Failed"));
@@ -80,6 +56,33 @@ public class Server{
 				ioException.printStackTrace();
 			}
 			handler.sendMessage(handler.obtainMessage(1,0,0,"Connection Closed"));
+		}
+	}
+		
+	private void readMessage(Handler handler)
+	{
+		try {
+		switch (in.readByte()) {
+		case 7:
+			int X,Y;
+			int numBytes = in.readByte() - 4;
+			X = in.readByte() + in.readByte() * 256;
+			Y = in.readByte() + in.readByte() * 256;
+			
+			handler.sendMessage(handler.obtainMessage(4,X,Y));
+			
+			while (numBytes > 0)
+			{
+				X += in.readByte();
+				Y += in.readByte();
+				handler.sendMessage(handler.obtainMessage(5,X,Y));
+				numBytes -= 2;
+			}
+		}
+		}
+		catch(IOException ioException){
+			handler.sendMessage(handler.obtainMessage(1,0,0,"Connection Failed"));
+			// ioException.printStackTrace();
 		}
 	}
 	
@@ -103,71 +106,5 @@ public class Server{
 	};
 	
 	public Handler getHandler() {return handler2;}
-	
-	void sendMessage(String msg)
-	{
-		try{
-			out.writeObject(msg);
-			out.flush();
-			System.out.println("server>" + msg);
-		}
-		catch(IOException ioException){
-			ioException.printStackTrace();
-		}
-	}
-	
-	public static void sendMove(int X, int Y)
-	{
-		if (toSend) sendData();
-		points[0] = (byte) (X % 256);
-		points[1] = (byte) (X / 256);
-		points[2] = (byte) (Y % 256);
-		points[3] = (byte) (Y / 256);
-		lastX = X;
-		lastY = Y;
-		nextPoint = 4;
-	}
-
-	public static void addPoint(int X, int Y)
-	{
-		points[nextPoint++] = (byte)(X-lastX);
-		lastX = X;
-		points[nextPoint++] = (byte)(Y-lastY);
-		lastY = Y;
-		toSend = true;
-		if (nextPoint > 12) sendMove(X,Y); // send this and start a new one
-	}
-	
-	public static void sendPoints(int X, int Y)
-	{
-		if (nextPoint >=4)
-		{
-		points[nextPoint++] = (byte)(X-lastX);
-		lastX = X;
-		points[nextPoint++] = (byte)(Y-lastY);
-		lastY = Y;
-		toSend = true;
-		sendMove(X,Y); // pen up action - a bit hacky to set these coords - perhaps rethink	- should be overwritten		
-		}
-	}
-		
-	public static void sendData()	
-	{	
-		if (connection != null)
-		{
-			try{
-				out.writeByte(7);
-				out.writeByte(nextPoint);
-				out.write(points,0,nextPoint);
-				out.flush();
-				Log.d("Server","sent points:" + nextPoint);
-			}
-			catch(IOException ioException){
-				ioException.printStackTrace();
-			}
-		}
-		toSend = false;
-	}
-	
 	
 }
--- WBD/src/uk/org/darnell/wbd/WBDActivity.java
+++ WBD/src/uk/org/darnell/wbd/WBDActivity.java
@@ -1,22 +1,20 @@
 package uk.org.darnell.wbd;
 
-import java.util.ArrayList;
-
 import uk.org.darnell.socket.Server;
 import uk.org.darnell.wbd.util.SystemUiHider;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
-import android.os.Build;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -32,6 +30,8 @@ public class WBDActivity extends Activity {
 	
 	 private final static String TAG = "WBD";
      private static GraphicsView gv; 
+ 	final Server server = new Server();
+ 	Thread th;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,40 +43,101 @@ public class WBDActivity extends Activity {
         gv = new GraphicsView(this);
         setContentView(gv);
         gv.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        
+		th = new Thread (new Runnable() 
+		{public void run() {server.run(handler);}});
+		th.start();
+        
     }
     
 	public class GraphicsView extends View {
-
-		private ArrayList<Integer> redraw;
-		private Canvas canvas = null;
-		private Bitmap bitMap;
+		
+		private Path path = new Path();
+		private Paint paint = new Paint();
+		private static final float STROKE_WIDTH = 3f;
+		/** Need to track this so the dirty region can accommodate the stroke. **/
+		private static final float HALF_STROKE_WIDTH = STROKE_WIDTH / 2;
 
 		public GraphicsView(Context context) {
 			super(context);
+		    paint.setAntiAlias(true);
+		    paint.setColor(Color.WHITE);
+		    paint.setStyle(Paint.Style.STROKE);
+		    paint.setStrokeJoin(Paint.Join.ROUND);
+		    paint.setStrokeWidth(STROKE_WIDTH);
+			// TODO Auto-generated constructor stub
 		}
+		
+		  /**
+		   * Optimizes painting by invalidating the smallest possible area.
+		   */
+		  private float lastX;
+		  private float lastY;
+		  private final RectF dirtyRect = new RectF();
+
+		  /**
+		   * Erases the signature.
+		   */
+		  public void clear() {
+		    path.reset();
+		    // Repaints the entire view.
+		    invalidate();
+		  }
 
 		protected void reset()
 		{	
 			Log.d(TAG,"reset()");
-			redraw = new ArrayList<Integer>();
-			// drawAll();
+			clear();
 		}
 
-		@Override
-		protected void onDraw(Canvas canvas1){ 
-			if (canvas == null)
-			{
-				Bitmap.Config conf = Bitmap.Config.ARGB_8888;
-				bitMap = Bitmap.createBitmap(1920, 1080, conf); 
-				canvas = new Canvas(bitMap);
-				reset();
-			}
-			// drawList();
-			Paint paint = new Paint();
-			canvas1.drawBitmap(bitMap, 0, 0, paint);	
-			redraw = new ArrayList<Integer>();
-		}
+		  @Override
+			public void onDraw(Canvas canvas) {
+			    canvas.drawPath(path, paint);
+			  }
 
+			  public boolean drawLine(int type,int X,int Y) {
+				    switch (type) {
+				      case 4:
+				        resetDirtyRect(X, Y);
+				        path.moveTo(X, Y);
+				        break;
+				      case 5:
+				        // When the hardware tracks events faster than they are delivered, the
+				        // event will contain a history of those skipped points.
+				        path.lineTo(X, Y);
+				        resetDirtyRect(X,Y);
+				        // Include half the stroke width to avoid clipping.
+					    invalidate(
+						        (int) (dirtyRect.left - HALF_STROKE_WIDTH),
+						        (int) (dirtyRect.top - HALF_STROKE_WIDTH),
+						        (int) (dirtyRect.right + HALF_STROKE_WIDTH),
+						        (int) (dirtyRect.bottom + HALF_STROKE_WIDTH));
+				        break;
+				      default:
+				        Log.d(TAG,"Ignored touch event: " + X + "," + Y);
+				        return false;
+				    }
+				    lastX = X;
+				    lastY = Y;
+
+				    
+				    return true;
+			  }
+
+			  /**
+			   * Resets the dirty region when the motion event occurs.
+			   */
+			  private void resetDirtyRect(float eventX, float eventY) {
+
+			    // The lastTouchX and lastTouchY were set when the ACTION_DOWN
+			    // motion event occurred.
+			    dirtyRect.left = Math.min(lastX, eventX);
+			    dirtyRect.right = Math.max(lastX, eventX);
+			    dirtyRect.top = Math.min(lastY, eventY);
+			    dirtyRect.bottom = Math.max(lastY, eventY);
+			  }
+
+			  /*
 		private void cls()
 		{
 			Paint paint = new Paint();
@@ -85,6 +146,7 @@ public class WBDActivity extends Activity {
 			bMap = Bitmap.createBitmap(1920, 1080, conf); 
 			canvas.drawBitmap(bMap, 0, 0, paint);
 		}
+		*/
 	}
 	
 	protected static Handler handler = new Handler()
@@ -98,16 +160,8 @@ public class WBDActivity extends Activity {
 				Toast.makeText(gv.getContext(), (String)msg.obj, Toast.LENGTH_SHORT).show();
 				Log.d(TAG, "Made Toast");
 			}
-			else if (msg.what == 2)
-			{
-				Toast.makeText(gv.getContext(), (String)msg.obj, Toast.LENGTH_SHORT).show();
-				//gv.setBackgroundImage(Server.screen);
-			}
-			else if (msg.what == 4) Server.sendMove(msg.arg1,msg.arg2);
-			else if (msg.what == 5) Server.addPoint(msg.arg1,msg.arg2);
-			else if (msg.what == 6) Server.sendPoints(msg.arg1,msg.arg2);
+			else if (msg.what == 4) gv.drawLine(4,msg.arg1,msg.arg2);
+			else if (msg.what == 5) gv.drawLine(5,msg.arg1,msg.arg2);
 		}
-	};
-	
-    
+	};    
 }
